0d0bdd299e49696d49f225188d1e25a1ad18a44d
kadelcode <kadelcode@gmail.com>
2022-10-01 20:20:42 +0100

    Identifiers

---
+++ Basic_Syntax/identifier.dart
@@ -0,0 +1,11 @@
+/* 
+Identifiers are the name which is used to define variables, methods, class, and function.
+ */
+
+void main() {
+  // Initializing identifiers
+  var f_name = "Ayodele";
+  var l_name = "Kadiri";
+  print("My fullname is ${f_name} ${l_name}");
+  
+}
