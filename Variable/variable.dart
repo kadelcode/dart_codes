fa4662096ded4ad56cee40d8280a52fcc7afc57f
kadelcode <kadelcode@gmail.com>
2022-10-01 23:04:23 +0100

    Dart Variables

---
+++ Variable/variable.dart
@@ -0,0 +1,11 @@
+void main() {
+  var name = 'Kadel';
+  int i,j,k; // Declaring Multiple variables
+
+  // Type annotation
+  int num = 35; // Type annotation
+
+  final String answer = "yes";
+
+  const double pi = 3.142;
+}
