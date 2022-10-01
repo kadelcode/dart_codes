d847ac3f7ee9becf06b86be66ce15cd98a8054a8
kadelcode <kadelcode@gmail.com>
2022-10-01 22:20:12 +0100

    Data types

---
+++ Data_Types/datatypes.dart
@@ -0,0 +1,30 @@
+// main function
+void main() {
+  // Number - stores numeric values
+  int val = 23;
+
+  // Double - declares double-precision variables
+  double v = 4.19;
+
+  // Strings - sequence of characters
+  var text = "Dart programming is fun!";
+
+  // Boolean - represents two values: true and false
+  bool i_istrue = true;
+  bool i_isfalse = false;
+
+  // Lists - a collection of multiple values in a single variable
+  var colors = ["White", "Blue", "Gold", "Purple"];
+
+  // Maps - values in key-value pairs(like a dictionary)
+  var student = {'first_name': 'Ayodele', 'last_name': 'Kadiri'};
+
+  // Runes - special string of Unicode UTF-32
+  var love_emoji = "\u2665";
+  var smile_emoji = "\u{1f600}";
+
+  print(love_emoji);
+  print(smile_emoji);
+
+
+}
