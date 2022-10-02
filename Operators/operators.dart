d2cde0adf374041264679ab4dd16a2fac8eb647f
kadelcode <kadelcode@gmail.com>
2022-10-02 21:48:56 +0100

    Working with operators

---
+++ Operators/operators.dart
@@ -0,0 +1,40 @@
+// DART OPERATORS
+
+// Declaring variables
+int a = 5;
+int b = 4;
+var c;
+void main() {
+  /* ______________ARITHMETIC OPERATORS_________________*/
+  // Addition
+  c = a + b;
+  
+  // Subtraction
+  c = a - b;
+
+  // Unary minus
+  c = ~a;
+
+  // Multiplication
+  c = a * b;
+  
+  // Division
+  c = a / b; // outputs a fractional value
+  c = a ~/ b; // outputs an integer value
+
+  // Modulus
+  c = a % b;
+
+
+
+  /* ______________ASSIGNMENT OPERATORS_________________*/
+  // Assigning variables x and y
+  int x = 2;
+  int y = 3;
+
+  x += y; // x = x + y;
+  x -= y; // x = x - y;
+
+  print(x);
+  print(c);
+}
