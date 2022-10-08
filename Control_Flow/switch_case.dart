917c607505d33d42532f9aacfd1fd4671c0b0004
kadelcode <kadelcode@gmail.com>
2022-10-08 22:45:12 +0100

    Switch case statement

---
+++ Control_Flow/switch_case.dart
@@ -0,0 +1,23 @@
+int n = 0; // Initialization
+void main() {
+  switch(n) {
+    case 1: {
+      print("n is 1");
+      break;
+    }
+
+    case 2: {
+      print("n is 2");
+      break;
+    }
+
+    case 3: {
+      print("n is 3");
+      break;
+    }
+
+    default: {
+      print("n is not 1,2,or 3");
+    }
+  }
+}
