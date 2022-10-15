ca889d4b270fbedcf019cc6c9b9477ae2fa4c7b2
kadelcode <kadelcode@gmail.com>
2022-10-15 20:44:38 +0100

    for...in loop

---
+++ Loops/for_in_loop.dart
@@ -0,0 +1,14 @@
+// main function
+int main()
+{
+  // initializing a list of numbers
+  var num_list = [44,33,20,4,30];
+
+  for (var i in num_list)
+  {
+    print(i);
+  }
+
+  // End program execution
+  return (0);
+}
