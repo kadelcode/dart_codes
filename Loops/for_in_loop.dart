7901ecb51178011747602c8646278dc0e8adccbf
kadelcode <kadelcode@gmail.com>
2022-10-16 21:28:43 +0100

    more on for...in loop

--- Loops/for_in_loop.dart
+++ Loops/for_in_loop.dart
@@ -3,11 +3,17 @@ int main()
 {
   // initializing a list of numbers
   var num_list = [44,33,20,4,30];
+  int sum = 0;
 
   for (var i in num_list)
   {
     print(i);
   }
+  for (var n in num_list) {
+    sum = sum + n;
+  }
+
+  print('The sum of numbers equals ${sum}');
 
   // End program execution
   return (0);
