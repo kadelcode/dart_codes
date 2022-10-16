809b4c3300d9096481e62fc2a8318a9c3f087d2e
kadelcode <kadelcode@gmail.com>
2022-10-16 21:57:05 +0100

    while loop modified

--- Loops/while_loop.dart
+++ Loops/while_loop.dart
@@ -4,9 +4,16 @@ int main()
   int i = 0;
 
   // while loop
-  while (i < 10)
+  // while (i < 10)
+  // {
+  //   print(i);
+  //   i++;
+  // }
+
+
+  while (i < 98)
   {
-    print(i);
+    print("${(i / 10).floor()}${i % 10}");
     i++;
   }
   return 0;
